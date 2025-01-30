3e19ea968964d82cbf5f6db6f02034b86f7bf1f1
Betsegaw Mesele <146959506+bexeGrace@users.noreply.github.com>
2025-01-30 15:05:21 +0300

    fixed some issues

--- LinearRegression/batch_GD.py
+++ LinearRegression/batch_GD.py
@@ -8,7 +8,7 @@ def batch_gradient(examples):
     num_features = len(examples[0][0])  # Number of features
     parameters = [0] * num_features  # Initialize parameters to zero
 
-    max_iterations = 1000  # Define inside function
+    max_iterations = 100000  # Define inside function
     alpha = 0.0000001
 
     for iteration in range(max_iterations):
@@ -17,23 +17,33 @@ def batch_gradient(examples):
         for j in range(num_features):  # Update each parameter
             gradient_sum = 0
             for example in examples:
-                h = hypothesis(example, new_parameters)  # Prediction
-                y = example[1]  # Actual value
+                h = hypothesis(example, parameters)  # Prediction
+                y = example[1]
                 x_j = example[0][j]  # Feature value
-                gradient_sum += (x_j/len(examples)) * (y - h)
+                gradient_sum += x_j * (y - h)
 
-            new_parameters[j] = parameters[j] + alpha * gradient_sum  # Update rule
+            new_parameters[j] = parameters[j] + (alpha/len(examples)) * gradient_sum  # Update rule
 
         parameters = new_parameters  # Apply new parameters
 
     return parameters
 
 examples = [
-    [[2104, 3], 400],
-    [[1600, 3], 330],
-    [[2400, 3], 369],
-    [[1416, 2], 232],
-    [[3000, 4], 540],
+    [[2, 3, 7], 3],
+    [[4, 2, 8], 7],
+    [[9, 8, 5], 5],
+    [[1, 5, 3], 9],
+    [[2, 3, 5], 4],
+    [[3, 2, 4], 8],
+    [[5, 2, 3], 6],
+    [[4, 1, 2], 7],
+    [[2, 4, 3], 10],
+    [[7, 5, 3], 6],
+    [[1, 3, 7], 4],
+    [[4, 2, 4], 8],
+    [[8, 5, 6], 5],
+    [[2, 9, 4], 11],
+    [[3, 2, 2], 6],
 ]
 
 model = batch_gradient(examples)
@@ -46,5 +56,5 @@ def applyBD(testSet):
 
     return sum
 
-print(applyBD([1000, 5]))
+print(applyBD([3, 2, 2]))
         
